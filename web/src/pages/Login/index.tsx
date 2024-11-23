148f39e069e8724e7c693660aa8d35f8b3a06407
Rodrigo Gouveia <rgestevao@gmail.com>
2024-11-23 10:02:13 -0300

    feat: finishing login layout

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -3,6 +3,7 @@ import LoginImage from "../../assets/Login-image.png";
 import Icon from "../../assets/favicon.svg";
 import { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   const [hidePassword, setHidePassword] = useState(true);
@@ -29,41 +30,53 @@ export default function Login() {
         <div className="header-container">
           <img src={Icon} />
           <span>Patas Felizes</span>
+
+          <div className="description">
+            Bem vindo(a) novamente, faça o seu login.
+          </div>
         </div>
 
-        <div className="description">
-          Bem vindo(a) novamente, faça o seu login.
+        <div className="content-container">
+          <div className="input-container">
+            <input type="text" placeholder="Digite o seu e-mail" />
+          </div>
+
+          <div className="input-container">
+            <input
+              id="input-password"
+              type="password"
+              placeholder="Digite a sua senha"
+            />
+
+            {hidePassword ? (
+              <span
+                id="spanId"
+                className="show-hide-password"
+                onClick={handleShowOrHidePassword}
+              >
+                <AiOutlineEyeInvisible className="icon-show-hide" />
+              </span>
+            ) : (
+              <span
+                id="spanId"
+                className="show-hide-password"
+                onClick={handleShowOrHidePassword}
+              >
+                <AiOutlineEye className="icon-show-hide" />
+              </span>
+            )}
+          </div>
+
+          <Link to={"/"} className="recovery-password-link">
+            Esqueceu sua senha?
+          </Link>
+
+          <button>Login</button>
         </div>
 
-        <div className="input-container">
-          <input type="text" placeholder="Digite o seu e-mail" />
-        </div>
-
-        <div className="input-container">
-          <input
-            id="input-password"
-            type="password"
-            placeholder="Digite a sua senha"
-          />
-
-          {hidePassword ? (
-            <span
-              id="spanId"
-              className="show-hide-password"
-              onClick={handleShowOrHidePassword}
-            >
-              <AiOutlineEyeInvisible className="icon-show-hide" />
-            </span>
-          ) : (
-            <span
-              id="spanId"
-              className="show-hide-password"
-              onClick={handleShowOrHidePassword}
-            >
-              <AiOutlineEye className="icon-show-hide" />
-            </span>
-          )}
-        </div>
+        <Link to={"/"} className="register-account-link">
+          Não possui conta? Crie uma agora mesmo
+        </Link>
       </form>
     </div>
   );
